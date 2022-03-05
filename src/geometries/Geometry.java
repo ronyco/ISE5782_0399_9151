d2759ab292f412e4e430dd2f0525becd211653e7
menachemtr96@gmail.com <menachemtr96@gmail.com>
2022-03-05 20:15:18 +0200

    fix

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,9 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * interface Geometry implements Geometric objects and function
+ */
 public interface Geometry {
     public Vector getNormal(Point p);
 }
