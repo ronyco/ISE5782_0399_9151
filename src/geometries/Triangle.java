6ddb36f7ca64125a7a711aa97d527930a7e29c2d
roncogen <roncohen@g.jct.ac.il>
2022-03-05 20:32:44 +0200

    Adding comment triangle constructor

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -6,6 +6,12 @@ import primitives.Vector;
  * Triangle class represents two-dimensional Triangle in 3D Cartesian coordinate
  */
 public class Triangle extends Polygon {
+    /**
+     * Triangle Constructor with 3 points
+     * @param p1 First Point value
+     * @param p2 Second Point value
+     * @param p3 Third Point value
+     */
     public Triangle(Point p1, Point p2, Point p3) {
         super(p1,p2,p3);
     }
