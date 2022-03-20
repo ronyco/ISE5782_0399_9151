4a9d5c157a570b8392bc7aaabe066481aa1e261c
menachemtr96@gmail.com <menachemtr96@gmail.com>
2022-03-20 22:58:31 +0200

    geomitries intersections tests

--- src/unittests/geometries/GeometriesTest.java
+++ src/unittests/geometries/GeometriesTest.java
@@ -24,7 +24,7 @@ class GeometriesTest {
     @Test
     public void findIntersections() {
         Geometries geometries = new Geometries(
-                new Sphere(new Point(0, 0, 2), 0.5),
+                new Sphere(new Point(1, 0, 0), 1),
                 new Polygon(
                         new Point( 1, 0, 0),
                         new Point(0,  1, 0),
@@ -41,24 +41,23 @@ class GeometriesTest {
 
         // ============ Equivalence Partitions Tests ==============
         //TC01: A few geometries intersects
-        result=geometries.findIntersections(new Ray(new Point(-1,-1,-1),new Vector(2,2,2)));
-        assertEquals(2, result.size(), "A few geometries intersects");
+        result=geometries.findIntersections(new Ray(new Point(0.25,-0.25,-1),new Vector(0,0,1)));
+        assertEquals(3, result.size(), "A few geometries intersects");
 
         // =============== Boundary Values Tests ==================
         //TC02: All geometries intersects
-        result=geometries.findIntersections(new Ray(new Point(0.2,0.2,-0.6),new Vector(0,0,1)));
+        result=geometries.findIntersections(new Ray(new Point(0.25,0.25,-1),new Vector(0,0,1)));
         assertEquals(4,result.size(),"All geometries intersects");
 
         //TC03: Only 1 geometry intersect
-        result=geometries.findIntersections(new Ray(new Point(0.2,0.2,0.2),new Vector(1,1,1)));
+        result=geometries.findIntersections(new Ray(new Point(-0.5,0,-1),new Vector(0,0,1)));
         assertEquals(1,result.size(),"Only 1 geometry intersect");
 
         //TC04: No geometries intersects
-        assertNull(geometries.findIntersections(new Ray(new Point(1, 1, 1), new Vector(1, 1, 1))), "No geometries intersects");
+        assertNull(geometries.findIntersections(new Ray(new Point(-2, 0, -1), new Vector(0,0,1))), "No geometries intersects");
 
         //TC05: Empty list of geometries
         assertNull(new Geometries().findIntersections(new Ray(new Point(1,2,3), new Vector(2,2,2))), "Empty list of geometries");
-
-
+        
     }
 }
